b5df44f3f2528fa75edeb886e33becf3e464f032
C4terp1llar <m0skvitin@mail.ru>
2024-06-17 02:26:50 +0300

    +стилизация

--- components/Chat.vue
+++ components/Chat.vue
@@ -1,28 +1,38 @@
 <template>
   <v-container>
-    <v-list v-if="messages.length" class="pa-3 mb-3">
-      <v-list-item v-for="(message, index) in messages" :key="index">
-        <v-list-item-content>{{ message }}</v-list-item-content>
+    <v-list class="mb-3 list">
+      <v-list-item v-if="!messages.length">
+        Сообщений пока нет
+      </v-list-item>
+      <v-list-item
+        v-for="(message, index) in messages"
+        :key="index"
+        :class="isMyMessage(message) ? 'my-message' : 'other-message'"
+        class="message-item ma-3"
+      >
+        <v-list-item-content>
+          <h2>{{ isMyMessage(message) ? 'Вы:' : `${message.user}:` }}</h2>
+          {{ message.text }}
+        </v-list-item-content>
       </v-list-item>
     </v-list>
-    <v-form v-if="!entered">
+    <v-form v-if="!entered" @submit.prevent="confirmName" class="d-flex">
       <v-text-field
         v-model="userName"
         label="Введите ваше имя, чтобы продолжить"
         autocomplete="off"
         outlined
-      >
-      </v-text-field>
-      <v-btn @click="confirmName" color="primary">Войти</v-btn>
+      ></v-text-field>
+      <v-btn @click="confirmName" x-large color="primary">Войти</v-btn>
     </v-form>
-    <v-form @submit.prevent="sendMessage" v-if="entered">
+    <v-form @submit.prevent="sendMessage" v-if="entered" class="d-flex">
       <v-text-field
         v-model="message"
         label="Введите ваше сообщение"
         autocomplete="off"
         outlined
       ></v-text-field>
-      <v-btn @click="sendMessage" color="primary">Отправить</v-btn>
+      <v-btn @click="sendMessage" x-large color="primary">Отправить</v-btn>
     </v-form>
   </v-container>
 </template>
@@ -31,26 +41,31 @@
 import { ref, onMounted } from 'vue';
 import { useSocket } from '@/composables/useSocket';
 
-const message = ref<String>('');
-const userName = ref<String>('');
+const message = ref<string>('');
+const userName = ref<string>('');
 
-const messages = ref<String[]>([]);
+const messages = ref<Array<{ user: string, text: string }>>([]);
 const socket = useSocket();
 
-const entered = ref<Boolean>(false);
+const entered = ref<boolean>(false);
 
 const sendMessage = () => {
   if (message.value.trim() !== '') {
-    socket.emit('chat message', `${userName.value}: ${message.value}`);
+    const msg = { user: userName.value, text: message.value };
+    socket.emit('chat message', msg);
     message.value = '';
   }
 };
 
-const confirmName = () =>{
+const confirmName = () => {
   if (userName.value.trim() !== '') {
-    entered.value = true
+    entered.value = true;
   }
-}
+};
+
+const isMyMessage = (msg: any) => {
+  return msg.user === userName.value;
+};
 
 onMounted(() => {
   socket.on('chat message', (msg) => {
@@ -60,5 +75,34 @@ onMounted(() => {
 </script>
 
 <style scoped>
+form {
+  gap: 15px;
+}
 
+.list {
+  height: 500px;
+  overflow-y: scroll;
+}
+
+.my-message {
+  background-color: #1976d2;
+  color: white;
+  margin-right: auto;
+}
+
+.other-message {
+  background-color: #e0e0e0;
+  color: black !important;
+  margin-left: auto;
+}
+
+.list::-webkit-scrollbar {
+  width: 8px;
+}
+.list::-webkit-scrollbar-track {
+  background: black;
+}
+.list::-webkit-scrollbar-thumb {
+  background-color: #888;
+}
 </style>
